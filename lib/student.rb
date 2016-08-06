0344af8b2933794981467f612f968a1b2904a077
Charlie Cook <turingbombe@gmail.com>
2016-08-06 14:44:35 -0400

    Complete Update labe

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,64 @@
 require_relative "../config/environment.rb"
+require 'pry'
 
 class Student
-
+  attr_accessor :name, :grade
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def initialize(id=nil,name,grade)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT)
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS students")
+  end
+
+  def save
+    if self.id != nil
+      self.update
+    else
+      sql= <<-SQL
+      INSERT INTO students (name, grade) VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql, self.name,self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    Student.new(name,grade).save
+  end
+
+  def update
+    sql = <<-SQL
+    UPDATE students SET name = ?, grade = ?
+    WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade,self.id)
+  end
+
+  def self.new_from_db(row)
+    Student.new(row[0],row[1],row[2])
+  end
+
+  def self.find_by_name(name)
+    sql= <<-SQL
+    SELECT * FROM students WHERE name= ?
+    SQL
+    new_from_db(DB[:conn].execute(sql,name).first)
+  end
 
 end
